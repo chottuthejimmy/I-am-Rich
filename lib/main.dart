a90a0bca857dc6741bb43529c9f941b9188f0af8
Tarun <srfashions.tarun@gmail.com>
2023-02-04 17:56:56 +0530

    gradient added

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,39 @@ void main() {
     MaterialApp(
       title: 'I am Rich',
       home: Scaffold(
-        backgroundColor: const Color(0xffF48FB1),
+        // backgroundColor: const Color(0xffF48FB1),
         appBar: AppBar(
           title: const Center(
             child: Text('I am Rich'),
           ),
-          backgroundColor: const Color(0xff9C27B0),
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.pink, Colors.purple],
+              ),
+            ),
+          ),
         ),
-        body: const Center(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xffF48FB1),
+                Color(0xffD270F0),
+              ],
+            ),
+          ),
+        child: const Center(
           child: Image(
             image: AssetImage("images/Kuber.png"),
           ),
         ),
       ),
     ),
+  ),
   );
 }
